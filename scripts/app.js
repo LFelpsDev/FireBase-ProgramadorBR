5d523039c6f133b53619c63a6aafeb80929c3032
Luis Felipe Garção <felipe-mara2003@hotmail.com>
2021-07-24 09:29:39 -0300

    Permission, rules, security, login and logout, funciton onAuthStateChanged and gerenciamento login

--- scripts/app.js
+++ scripts/app.js
@@ -10,17 +10,52 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 
-const newUserEmail = "felipe-mara2003@hotmail.com"
-const newUserPassword = "LFelpsDev";
+let auth = firebase.auth();
 
-let auth = firebase.auth()
 
-auth.createUserWithEmailAndPassword(newUserEmail, newUserPassword)
-  .then(user => {
-    console.log(user);
+let user = auth.currentUser;
+
+function login() {
+  let userEmail = "felipe-mara2003@hotmail.com";
+  let userPassword = "789159";
+
+  auth.setPersistence(firebase.auth.Auth.Persistence.NONE).then(() => {
+    // local padrão, 
+    //para indicar que vamos usar o login somente um pagina  SESSION,
+    // NONE VAI DESLOGAR, So Serve 
+    auth
+      .signInWithEmailAndPassword(userEmail, userPassword)
+      .then((loggedUser) => {
+        console.log(auth.currentUser);
+      })
+      .catch((error) => {
+        console.log("Error", error);
+      });
   }).catch(error => {
     console.log(error);
   })
 
 
+}
 
+login()
+
+auth.onAuthStateChanged(user => {
+  if (user){
+    console.log(user)
+  }else {
+    console.log("Ningum Logado")
+  }
+
+  
+})
+
+function logout(){
+  auth.signOut().then(() => {
+    console.log('USUARIO DESLOGADO')
+  }).catch(error => {
+    console.log(error)
+  })
+}
+
+setTimeout(login, 2000); // login para logar, logout para deslogar
